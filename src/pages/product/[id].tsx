bd81010e3c5cd6e9e564a49f8aa9ba53e0747179
MarcosMDev <marcosmagnopb@gmail.com>
2022-12-13 20:16:20 -0300

    add static props and static paths

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,11 @@
 import axios from 'axios'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
 import {
     ImageContainer,
     ProductContainer,
@@ -42,6 +45,12 @@ export default function Product({ product }: ProductProps) {
         }
     }
 
+    const { isFallback } = useRouter()
+
+    if (isFallback) {
+        return <p>Loading. . .</p>
+    }
+
     return (
         <>
             <Head>
@@ -71,4 +80,40 @@ export default function Product({ product }: ProductProps) {
             </ProductContainer>
         </>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [{ params: { id: 'prod_Ms74cXahAkkPgG' } }],
+        fallback: true,
+    }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+    params,
+}) => {
+    const productId = params!.id
+
+    const product = await stripe.products.retrieve(productId, {
+        expand: ['default_price'],
+    })
+
+    const price = product.default_price as Stripe.Price
+
+    return {
+        props: {
+            product: {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0],
+                price: new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                }).format(price.unit_amount! / 100),
+                description: product.description,
+                defaultPriceId: price.id,
+            },
+        },
+        revalidate: 60 * 60 * 1, // 1 hour
+    }
 }
