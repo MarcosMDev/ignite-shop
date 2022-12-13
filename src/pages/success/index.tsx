c449dafaf0ade53573ed37cea4c78488c66ae36f
MarcosMDev <marcosmagnopb@gmail.com>
2022-12-13 20:44:07 -0300

    success page finished

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -12,13 +12,10 @@ import {
 
 interface SuccessProps {
     customerName: string
-    product: {
-        name: string
-        imageUrl: string
-    }
+    lineItems: Stripe.LineItem[]
 }
 
-export default function Success({ customerName, product }: SuccessProps) {
+export default function Success({ customerName, lineItems }: SuccessProps) {
     return (
         <>
             <Head>
@@ -28,36 +25,27 @@ export default function Success({ customerName, product }: SuccessProps) {
             </Head>
             <SuccessContainer>
                 <Imagesontainer>
-                    <ImageContainer>
-                        <Image
-                            src={product.imageUrl}
-                            width={120}
-                            height={110}
-                            alt=""
-                        />
-                    </ImageContainer>
-                    <ImageContainer>
-                        <Image
-                            src={product.imageUrl}
-                            width={120}
-                            height={110}
-                            alt=""
-                        />
-                    </ImageContainer>
-                    <ImageContainer>
-                        <Image
-                            src={product.imageUrl}
-                            width={120}
-                            height={110}
-                            alt=""
-                        />
-                    </ImageContainer>
+                    {lineItems.map((lineItem) => {
+                        const product = lineItem.price
+                            ?.product as Stripe.Product
+
+                        return (
+                            <ImageContainer key={product.id}>
+                                <Image
+                                    src={product.images[0]}
+                                    width={120}
+                                    height={110}
+                                    alt=""
+                                />
+                            </ImageContainer>
+                        )
+                    })}
                 </Imagesontainer>
                 <h1>Compra efetuada</h1>
 
                 <p>
-                    Uhuul <strong>{customerName}</strong>, sua sua compra de 3
-                    camisetas já está a caminho da sua casa.
+                    Uhuul <strong>{customerName}</strong>, sua sua compra de{' '}
+                    {lineItems.length} camisetas já está a caminho da sua casa.
                 </p>
 
                 <Link href="/">Voltar ao catálogo</Link>
@@ -83,15 +71,12 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
     })
 
     const customerName = session.customer_details?.name
-    const product = session.line_items?.data[0].price?.product as Stripe.Product
+    const lineItems = session.line_items?.data
 
     return {
         props: {
             customerName,
-            product: {
-                name: product.name,
-                imageUrl: product.images[0],
-            },
+            lineItems,
         },
     }
 }
